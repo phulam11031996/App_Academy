a3531e0ca780b21a4c8d5816e666db958fdcc629
Phu Lam <phulam11031996@gmail.com>
2020-06-04 12:49:06 -0700

    update on map.rb

---
+++ ADTs/map.rb
@@ -0,0 +1,29 @@
+ class Map
+    def initialize
+        @map = []
+    end
+
+    def set(key, val)
+        map << [key, val]
+    end
+
+    def get(key)
+        map.each { |arr| return arr[1] if arr[0] == key }
+        nil
+    end
+
+    def delete(key)
+        map.each.with_index do |arr, idx|
+            map.delete_at(idx) if arr[0] == key
+            return map[idx] if arr[0] == key
+        end
+        nil
+    end
+
+    def show
+        p map
+    end
+
+    private 
+    attr_accessor :map
+ end
