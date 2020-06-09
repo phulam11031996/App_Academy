6f7085e3673fbc6a232bce022d4008b01d4e615c
Phu Lam <phulam11031996@gmail.com>
2020-06-08 18:43:40 -0700

    done

--- ADTs/LRU_Cache.rb
+++ ADTs/LRU_Cache.rb
@@ -1,6 +1,8 @@
   class LRUCache
     def initialize(cache_size = 5)
         @cache = Array.new()
+        @hash_set = Array.new(cache_size) {[]}
+        @cache_size = cache_size
     end
 
     def count
@@ -8,13 +10,14 @@
     end
 
     def add(el)
-      if @cache.include?(el)
+      if include_hash(el)
         @cache.delete(el)
         @cache.push(el)
       else
         @cache.unshift
         @cache.push(el)
       end
+      add_hash(el)
       el
     end
 
@@ -22,8 +25,21 @@
       @cache
     end
 
+    def add_hash(el)
+        hash_val = el.hash 
+        idx = hash_val % @cache_size
+        @hash_set[idx].push(el)
+    end
+
+    def include_hash(el)
+        idx = el.hash % @cache_size
+        @cache[idx].include?(el)
+    end
+
     # private
     # helper methods go here!
 
     attr_accessor :cache_size
+    attr_accessor :cache_size 
+    attr_accessor :hash_set
   end
